daf6d8e12d228783a2c2b4c4df1148cd34d4decf
yatinla <mike@taylorwebhome.org>
2013-01-06 19:43:39 -0800

    Fix pyflix_search_movies to handle the case where only one movie is returned.  Turns out the silly Netflix API returns it not in a list as it does if there are more than one matching instead of it still being a list but with only one item.  Grrrrrr

--- pyflix_search_movies.py
+++ pyflix_search_movies.py
@@ -12,25 +12,41 @@ import webbrowser
 from netflixauth import NetflixAuth
 import json
 
-fname = raw_input('Enter file name with auth info: ')
+if len(sys.argv) == 1:
+  # Try to get default file 
+  fname = 'authinfo'
 
-authdata = NetflixAuth.load( fname )
+try:
+  authdata = NetflixAuth.load( fname )
+except Exception as e:
+  print 'Default \'authinfo\' file not found'
+  try:
+    fname = raw_input('Enter file name with auth info: ')
+  except:
+    print 'Failed loading auth data from file ' + fname
+    print e
+    sys.exit(0)
 
 search_term = raw_input('Enter search term: ')
-
-print 'App name: ', authdata.appname
 
 netflix = NetflixAPIV2( authdata.appname, authdata.consumer_token, authdata.consumer_secret )
 
 movies = netflix.title_autocomplete(search_term)
 
-print 'Movies Titles with ' + search_term + ' in them: ' + "\n"
+print '\n\nMovies Titles with ' + search_term + ' in them: ' + "\n"
 
 f = open('search.json', 'w' )
 f.write(json.dumps( movies, sort_keys=False, indent=4, separators=(',', ': ')))
 f.close()
 
-for title in movies['autocomplete']['autocomplete_item']:
-    print '\t' + unicode(title['title']['short'])
+# Well, this is a pretty poor design choice.  If only
+# one result is returned then it isn't in an array!
+try:
+  for title in movies['autocomplete']['autocomplete_item']:
+      print '\t' + unicode(title['title']['short'])
+except:
+    print '\t' + unicode(movies['autocomplete']['autocomplete_item']['title']['short'])
+
+print ''
 
 
